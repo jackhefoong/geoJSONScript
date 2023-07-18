6eee537da998212729552987a5c6a10cbe0aeed2
jackhe <jackhe@whiteroom.work>
2023-07-18 13:58:18 +0800

    johor gneerate

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
 const districts = fs.readFileSync("List_of_districts_in_Malaysia.json", "utf8");
-const selangor = fs.readFileSync("selangor.json", "utf8");
+const johor = fs.readFileSync("johor.json", "utf8");
 const all = fs.readFileSync("all.geojson", "utf8");
 
 // const districtsJSON = JSON.parse(districts);
-const selangorJSON = JSON.parse(selangor);
-const selangorDistricts = selangorJSON.map((d) => d.districtName);
+const johorJSON = JSON.parse(johor);
+const johorDistricts = johorJSON.map((d) => d.districtName);
 
 const allJSON = JSON.parse(all);
 const allDisctricts = allJSON.features.map((d) => d);
@@ -15,7 +15,7 @@ let matchingDistricts = [];
 
 allDisctricts.forEach((district, i) => {
   const withSuffix = district.properties.shapeName + " District";
-  if (selangorDistricts.includes(withSuffix)) {
+  if (johorDistricts.includes(withSuffix)) {
     matchingDistricts.push(district);
   }
 });
@@ -25,4 +25,4 @@ const geoJSON = {
   features: matchingDistricts,
 };
 
-fs.writeFileSync("selangor.geojson", JSON.stringify(geoJSON));
+fs.writeFileSync("johor.geo.json", JSON.stringify(geoJSON));
